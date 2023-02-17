919cc0d8f2dc99d1b977546427f8698162e2ce17
YanaKochurko <88380247+YanaKochurko@users.noreply.github.com>
2023-02-17 12:36:58 +0300

    Add restdb.io integration

--- js/common.js
+++ js/common.js
@@ -8,5 +8,6 @@ function findGetParameter(parameterName) {
             tmp = item.split("=");
             if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
         });
+
     return result;
 }
